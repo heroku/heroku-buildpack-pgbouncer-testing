75bf2890bfaade65444ddca35c7274b046d5e4f5
Ben Fritsch <beanie@benle.de>
2021-01-26 16:31:24 +0100

    change test output

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ DB = Sequel.connect(ENV['DATABASE_URL'])
 get '/' do
   begin
     res = DB.run("SELECT NOW()")
-    "SELECT NOW() => #{res} executed against #{ENV['DATABASE_URL']}"
+    host = URI.parse(ENV['DATABASE_URL']).host
+    "executed against #{host}"
   rescue => e
     "exception happened #{e.message}"
   end
